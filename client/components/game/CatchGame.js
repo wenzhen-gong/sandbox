b6cd4d404562af34d7b5d6f2705c149969fdf3f7
Wenzhen Gong <kong20100902@hotmail.com>
2024-04-16 21:46:15 -0700

    catcher is now able to catch

--- client/components/game/CatchGame.js
+++ client/components/game/CatchGame.js
@@ -1,9 +1,18 @@
 import "../style.css";
+
 import bg1 from "../../assets/bg1.png";
+import e1 from "../../assets/e1.png";
+import e2 from "../../assets/e2.png";
+import p1 from "../../assets/p1.png";
+import p2 from "../../assets/p2.png";
+import p3 from "../../assets/p3.png";
+import p4 from "../../assets/p4.png";
+
 import React, { useRef, useEffect, useState } from "react";
-import { DropMovement } from "./DropMovement";
+
 import moveCatcher from "./moveCatcher";
-import { CANVASWIDTH } from "./sizeDef";
+import { CANVASWIDTH, CATCHERSIZE, DROPSIZE, Drop } from "./sizeDef";
+
 const CatchGame = () => {
   const convasRef = useRef(null);
   const startTime = Date.now();
@@ -11,11 +20,12 @@ const CatchGame = () => {
   const [myctx, setMyctx] = useState(null);
   const [canvasWidth, setCanvasWidth] = useState(null);
   const [canvasHeight, setCanvasHeight] = useState(null);
-  const [catcherX, setCatcherX] = useState(null);
+
+  let catcherX = useRef(0);
 
   const handleMouseMove = (x) => {
     moveCatcher(myctx, x, canvasWidth, canvasHeight);
-    setCatcherX(x);
+    catcherX.current = x;
   };
 
   useEffect(() => {
@@ -30,16 +40,43 @@ const CatchGame = () => {
     setCanvasWidth(canvas.width);
     setCanvasHeight(canvas.height);
 
-    // const drop = () => {
-    //   if (Date.now() - startTime <= 60000) {
-    //     DropMovement(myctx, canvas.width, canvas.height);
-    //   } else {
-    //     clearInterval(intervalID);
-    //   }
-    // };
-    // const intervalID = setInterval(drop, 1000);
-        DropMovement(myctx, canvas.width, canvas.height);
+    const render = (drop) => {
+      console.log(catcherX.current);
+      myctx.clearRect(drop.x, drop.y - 1, drop.size, drop.size);
+      myctx.drawImage(drop.img, drop.x, drop.y, drop.size, drop.size);
+      drop.y += 2;
+      if (drop.y <= canvas.height) {
+        if (
+          drop.y >= canvas.height - DROPSIZE - CATCHERSIZE &&
+          drop.y <= canvas.height - CATCHERSIZE &&
+          drop.x >= catcherX.current - DROPSIZE &&
+          drop.x <= catcherX.current + CATCHERSIZE
+        ) {
+          console.log("catch it");
+        } else {
+          requestAnimationFrame(() => render(drop));
+        }
+      }
+    };
 
+    const drop = () => {
+      if (Date.now() - startTime <= 60000) {
+        let myImg = new Image();
+        const imgCol = [e1, e2, p1, p2, p3, p4];
+        myImg.src = imgCol[Math.floor(Math.random() * imgCol.length)];
+
+        let myDrop = new Drop(
+          Math.floor(Math.random() * (canvas.width - DROPSIZE)),
+          -DROPSIZE,
+          DROPSIZE,
+          myImg
+        );
+        render(myDrop);
+      } else {
+        clearInterval(intervalID);
+      }
+    };
+    const intervalID = setInterval(drop, 1000);
   }, []);
 
   return (
--- client/components/game/sizeDef.js
+++ client/components/game/sizeDef.js
@@ -1,5 +1,12 @@
 const CANVASWIDTH = window.innerWidth;
 const DROPSIZE = CANVASWIDTH / 15;
 const CATCHERSIZE = CANVASWIDTH / 15;
-
-export {CANVASWIDTH, DROPSIZE, CATCHERSIZE}
+class Drop {
+  constructor(x, y, size, img) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.img = img;
+  }
+}
+export {CANVASWIDTH, DROPSIZE, CATCHERSIZE, Drop}
