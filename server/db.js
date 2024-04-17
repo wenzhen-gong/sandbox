4789f3049287cb16b50a839622c80a547a955788
Wenzhen Gong <kong20100902@hotmail.com>
2024-04-17 02:55:35 -0700

    use .env

--- server/db.js
+++ server/db.js
@@ -1,8 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
+require('dotenv').config()
 
-const URL = 'https://abyhqygnagfabbeaxciq.supabase.co';
-const KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFieWhxeWduYWdmYWJiZWF4Y2lxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzMzgxODQsImV4cCI6MjAyODkxNDE4NH0.WnD6QIfxz4w5U_os09GjdHrgjrF_HCyEUDhxi0hSeZw';
-
+const URL = process.env.URL;
+const KEY = process.env.KEY;
 const supabase = createClient(URL, KEY);
 
 module.exports = supabase;
